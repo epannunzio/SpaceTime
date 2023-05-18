1290fe48bc0791e955222ae90fd9bc2720cb7c73
Edilza Rodrigues Pannunzio <65309655+epannunzio@users.noreply.github.com>
2023-05-18 15:41:20 -0300

    Add files via upload

---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import { memoriesRoutes } from './routes/memories'
+
+const app = fastify()
+
+app.register(cors, {
+  origin: true,
+})
+app.register(memoriesRoutes)
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('🚀 HTTP server running on http://localhost:3333')
+  })
